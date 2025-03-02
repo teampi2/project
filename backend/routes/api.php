dc28050fecc10ae903eaf819c65d538551c3abfb
PabloKauan-TI <kpablo429@gmail.com>
2025-03-01 22:55:44 -0300

    feat/refactor_api_route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AdministratorController;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\ClassesController;
 use App\Http\Controllers\ClassStudentController;
+use App\Http\Controllers\ClassTeacherController;
 use App\Http\Controllers\CoordinatorController;
 use App\Http\Controllers\MonitorController;
 use App\Http\Controllers\SchoolController;
@@ -83,6 +84,14 @@ Route::prefix('classStudent')->middleware(['auth:sanctum', CheckRole::class.':AD
     Route::get('/showTurmasByUser', [ClassStudentController::class, 'showByTurmasForUser'])->middleware(['auth:sanctum', CheckRole::class.':ADMINISTRATOR|COORDENATOR|MONITOR|STUDENT']);
     Route::get('/showUsersByTurma', [ClassStudentController::class, 'showByUsersForTurma'])->middleware(['auth:sanctum', CheckRole::class.':ADMINISTRATOR|COORDENATOR|MONITOR|STUDENT']);
     Route::delete('/delete', [ClassStudentController::class, 'destroy']);
+});//Rotas Feitas
+
+Route::prefix('classTeacher')->middleware(['auth:sanctum', CheckRole::class.':ADMINISTRATOR|COORDENATOR|MONITOR'])->group(function () {
+    Route::post('/register', [ClassTeacherController::class, 'create']);
+    Route::put('/update', [ClassTeacherController::class, 'update']);
+    Route::get('/showTurmasByUser', [ClassTeacherController::class, 'showByTurmasForUser']);
+    Route::get('/showUsersByTurma', [ClassTeacherController::class, 'showByUsersForTurma'])->middleware(['auth:sanctum', CheckRole::class.':ADMINISTRATOR|COORDENATOR|MONITOR|STUDENT']);
+    Route::delete('/delete', [ClassTeacherController::class, 'destroy']);
 });//Rotas Feitas
 
 Route::prefix('activity')->middleware(['auth:sanctum', CheckRole::class.':ADMINISTRATOR|COORDENATOR|MONITOR'])->group(function () {
